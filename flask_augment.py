70e9bc78b936fb6401efc6631e2ac09fd3077b5f
Rahul Kumar <rahulsinner@gmail.com>
2011-11-30 17:55:57 +0530

    Refactoring for parseable errors.

--- flask_augment.py
+++ flask_augment.py
@@ -4,8 +4,22 @@ flask web framework.
 """
 import re
 from functools import wraps
+from collections import defaultdict
 
 from flask import request
+
+class AugmentError(ValueError):
+    """
+    Default exception raised when a contraint is voilated.
+    """
+    def __init__(self, errors):
+        self.errors = errors
+
+    def __str__(self):
+        """
+        Dumps the `self.errors` dictionary.
+        """
+        return repr(self.errors)
 
 def _propogate_error(errors, handler=None, exception_type=TypeError):
     """
@@ -25,17 +39,32 @@ def ensure_args(storage=request.args, error_handler=None, **rules):
         @wraps(fn)
         def wrapper(*args, **kwargs):
             results = _check_args(rules, storage)
-            errors = []
-            for arg_name, arg_val, valid in results:
-                if not valid:
-                    errors.append("'%s = %s' violates constraint %s. "
-                                  % (arg_name, arg_val, rules[arg_name]))
+            errors = _construct_errors(results, rules)
             if errors:
-                _propogate_error(''.join(errors), error_handler)
+                plural = 'errors' if len(errors) > 1 else 'error'
+                errors['base'].append('%s %s' % (len(errors, plural)))
+                _propogate_error(errors, error_handler)
             else:
                 return fn(*args, **kwargs)
         return wrapper
     return decorator
+
+def _construct_errors(results, rules):
+    """
+    Constructs errors dictionary from the returned results.
+    """
+    errors = defaultdict(list)
+    for res in results:
+        if len(res) == 4:
+            arg_name, arg_val, valid, message = res
+        else:
+            arg_name, arg_val, valid = res
+        if not valid:
+            if not message:
+                # No user supplied message. Construct a generic message.
+                message = '"%s" violates constraint "%s."' % (arg_val, rules[arg_name])
+            errors[arg_name].append(message)
+    return errors
 
 def _check_args(rules, storage):
     """
@@ -47,11 +76,21 @@ def _check_args(rules, storage):
         # Get the argument value.
         arg_val = storage.get(arg_name)
         if arg_val:
+            message = None
+            if isinstance(constraint, list) or isinstance(constraint, tuple):
+                if len(constraint) == 2:
+                    constraint, message = constraint
+                else:
+                    raise ValueError('Constraints can either be "(constraint, message)" or "constraint"'
+                                    '"%s" is in inproper format' % constraint)
             # `constraint` can either be a regex or a callable.
             validator = constraint
             if not callable(constraint):
                 validator = lambda val: re.match(constraint, str(val))
-            results.append((arg_name, arg_val, validator(arg_val)))
+            if message:
+                results.append((arg_name, arg_val, validator(arg_val), message))
+            else:
+                results.append((arg_name, arg_val, validator(arg_val)))
     return results
 
 def ensure_one_of(storage=request.args, error_handler=None, exclusive=False, **rules):
@@ -64,18 +103,23 @@ def ensure_one_of(storage=request.args, error_handler=None, exclusive=False, **r
         @wraps(fn)
         def wrapper(*args, **kwargs):
             results = _check_args(rules, storage)
-            valid_count = len([valid for arg_name, arg_val, valid in results
-                                if valid])
-            if valid_count < 1:
-                error_msg = "One of '%s' must validate. Constraints: %s" % \
-                        (rules.keys(), rules)
-                _propogate_error(error_msg, error_handler)
-            elif valid_count > 1 and exclusive:
-                error_msg = "Only one of '%s' must validate. Constraints: %s" % \
-                        (rules.keys(), rules)
-                _propogate_error(error_msg, error_handler)
+            errors = _construct_errors(results, rules)
+            if errors:
+                valid_count = len(rules) - len(errors)
+                if valid_count < 1:
+                    errors['base'].append('One of constraints must validate.')
+                    return _propogate_error(errors, error_handler)
+                elif valid_count > 1 and exclusive:
+                    errors['base'].append('Only one of constraints should validate.')
+                    return _propogate_error(errors, error_handler)
+                else:
+                    return fn(*args, **kwargs)
             else:
-                return fn(*args, **kwargs)
+                if exclusive:
+                    errors['base'].append('Only one of constraints should validate.')
+                    return _propogate_error(errors, error_handler)
+                else:
+                    return fn(*args, **kwargs)
         return wrapper
     return decorator
 
