5fc0fff27aa07d72e0d8e7cf5a59612ddad2f4ad
Rahul Kumar <rahulsinner@gmail.com>
2011-11-30 18:00:25 +0530

    Correcting github link.

--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@ Python decorators for contracts for flask web framework.
 
 from setuptools import setup
 setup(name='Flask-Augment',
-      version='0.3',
+      version='0.4',
       description='Python decorators implementing contracts for flask framework',
       long_description=__doc__,
       author='Rahul Kumar',
       author_email='rahul@thoughtnirvana.com',
       license='BSD',
-      url='https://github.com/thoughtnirvana/augment',
+      url='https://github.com/thoughtnirvana/flask-augment',
       py_modules=['flask_augment']
      )
